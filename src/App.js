e55e9830ccffde1d534c48f1078de2d9617f5fdb
felix <felix@volny.co>
2016-11-06 13:00:07 +0700

    using marked library

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 import marked from 'marked';
 
 class App extends Component {
+  convert() {
+    const raw = marked('This *is* __Markdown__.', { sanitze: true, gfm: true })
+    return { __html: raw }
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +18,7 @@ class App extends Component {
           <h2>Markdown Previewer</h2>
         </div>
         <p className="App-intro">
+          <div dangerouslySetInnerHTML={this.convert()} />
         </p>
       </div>
     );
